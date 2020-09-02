b0ed8f477e9fa1eae6bff7d38f2d83327e9219f3
Christian Lewis <christianlewis024@gmail.com>
2020-09-02 10:28:24 -0700

    changed the scaling and responsiveness of the boxes

--- src/App.js
+++ src/App.js
@@ -18,53 +18,52 @@ function App() {
             >
               <img src="hrfss.png" className="apiss"></img>{" "}
             </a>
-
-            <span className="link-span">
-              {" "}
-              <a
-                href="https://main.d2njpi9j1s76mb.amplifyapp.com/"
-                target="_blank"
-              >
-                Deployed <i class="fas fa-globe"></i>
-              </a>
-              <a
-                href="https://github.com/Lambda-School-Labs/Labs25-Human_Rights_First-TeamB-FE"
-                target="_blank"
-              >
-                Repository <i class="fab fa-github"></i>
-              </a>
-            </span>
-            <div className="proj-about">
-              here is where i will write about the skills i used to create the
-              project heasdfasfdasdfasdfsadfs where i will write about the
-              skills i used to create the project here is where i will write
-              about the skills i used to create the project here is where i will
-              write about the skills i used to create the project
-            </div>
+          </div>
+          <span className="link-span">
+            {" "}
+            <a
+              href="https://main.d2njpi9j1s76mb.amplifyapp.com/"
+              target="_blank"
+            >
+              Deployed <i class="fas fa-globe"></i>
+            </a>
+            <a
+              href="https://github.com/Lambda-School-Labs/Labs25-Human_Rights_First-TeamB-FE"
+              target="_blank"
+            >
+              Repository <i class="fab fa-github"></i>
+            </a>
+          </span>
+          <div className="proj-about">
+            here is where i will write about the skills i used to create the
+            project heasdfasfdasdfasdfsadfs where i will write about the skills
+            i used to create the project here is where i will write about the
+            skills i used to create the project here is where i will write about
+            the skills i used to create the project
           </div>
           <div className="project-2">
             <a href="https://www.sciencewithmrslewis.com/" target="_blank">
               <img src="chanelss.png" className="apiss"></img>
             </a>
-            <span className="link-span">
-              {" "}
-              <a href="https://www.sciencewithmrslewis.com/" target="_blank">
-                Deployed <i class="fas fa-globe"></i>
-              </a>
-              <a
-                href="https://github.com/christianlewis024/chanelv2"
-                target="_blank"
-              >
-                Repository <i class="fab fa-github"></i>
-              </a>
-            </span>
-            <div className="proj-about">
-              here is where i will write about the skills i used to create the
-              project here is where i will write about the skills i used to
-              create the project here is where i will write about the skills i
-              used to create the project here is where i will write about the
-              skills i used to create the project
-            </div>
+          </div>
+          <span className="link-span">
+            {" "}
+            <a href="https://www.sciencewithmrslewis.com/" target="_blank">
+              Deployed <i class="fas fa-globe"></i>
+            </a>
+            <a
+              href="https://github.com/christianlewis024/chanelv2"
+              target="_blank"
+            >
+              Repository <i class="fab fa-github"></i>
+            </a>
+          </span>
+          <div className="proj-about">
+            here is where i will write about the skills i used to create the
+            project here is where i will write about the skills i used to create
+            the project here is where i will write about the skills i used to
+            create the project here is where i will write about the skills i
+            used to create the project
           </div>
         </div>
         <div className="bottom2">
@@ -72,49 +71,49 @@ function App() {
             <a href="https://apimountain.netlify.app/" target="_blank">
               <img src="apiss.png" className="apiss"></img>
             </a>
-            <span className="link-span">
-              {" "}
-              <a href="https://apimountain.netlify.app/" target="_blank">
-                Deployed <i class="fas fa-globe"></i>
-              </a>
-              <a
-                href="https://github.com/christianlewis024/apimountain"
-                target="_blank"
-              >
-                Repository <i class="fab fa-github"></i>
-              </a>
-            </span>
-            <div className="proj-about">
-              here is where i will write about the skills i used to create the
-              project here is where i will write about the skills i used to
-              create the project here is where i will write about the skills i
-              used to create the project here is where i will write about the
-              skills i used to create the project
-            </div>
+          </div>
+          <span className="link-span">
+            {" "}
+            <a href="https://apimountain.netlify.app/" target="_blank">
+              Deployed <i class="fas fa-globe"></i>
+            </a>
+            <a
+              href="https://github.com/christianlewis024/apimountain"
+              target="_blank"
+            >
+              Repository <i class="fab fa-github"></i>
+            </a>
+          </span>
+          <div className="proj-about">
+            here is where i will write about the skills i used to create the
+            project here is where i will write about the skills i used to create
+            the project here is where i will write about the skills i used to
+            create the project here is where i will write about the skills i
+            used to create the project
           </div>
           <div className="project-4">
             <a href="https://weeklyhistoryquiz.netlify.app/" target="_blank">
               <img src="quizss.png" className="apiss"></img>
             </a>
-            <span className="link-span">
-              {" "}
-              <a href="https://weeklyhistoryquiz.netlify.app/" target="_blank">
-                Deployed <i class="fas fa-globe"></i>
-              </a>
-              <a
-                href="https://github.com/christianlewis024/historyquizfinal"
-                target="_blank"
-              >
-                Repository <i class="fab fa-github"></i>
-              </a>
-            </span>
-            <div className="proj-about">
-              here is where i will write about the skills i used to create the
-              project here is where i will write about the skills i used to
-              create the project here is where i will write about the skills i
-              used to create the project here is where i will write about the
-              skills i used to create the project
-            </div>
+          </div>
+          <span className="link-span">
+            {" "}
+            <a href="https://weeklyhistoryquiz.netlify.app/" target="_blank">
+              Deployed <i class="fas fa-globe"></i>
+            </a>
+            <a
+              href="https://github.com/christianlewis024/historyquizfinal"
+              target="_blank"
+            >
+              Repository <i class="fab fa-github"></i>
+            </a>
+          </span>
+          <div className="proj-about">
+            here is where i will write about the skills i used to create the
+            project here is where i will write about the skills i used to create
+            the project here is where i will write about the skills i used to
+            create the project here is where i will write about the skills i
+            used to create the project
           </div>
         </div>
       </div>
